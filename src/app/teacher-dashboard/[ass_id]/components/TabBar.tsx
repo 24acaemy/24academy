3885b1754071da1f861df693551598b200068a23
24acaemy <24academy.24online@gmail.com>
2025-09-05 14:39:15 +0300

    Update TabBar.tsx

--- src/app/teacher-dashboard/[ass_id]/components/TabBar.tsx
+++ src/app/teacher-dashboard/[ass_id]/components/TabBar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 interface TabBarProps {
-    activeTab: 'students' | 'lessons' | 'exams' | 'grades'; // Tab types for the course details
-    onTabChange: (tab: 'students' | 'lessons' | 'exams' | 'grades') => void; // Function to handle tab changes
-    dynamicTabs: { label: string; value: 'students' | 'lessons' | 'exams' | 'grades' }[]; // Dynamic tab options
+    activeTab: 'students' | 'lessons' ; // Tab types for the course details
+    onTabChange: (tab: 'students' | 'lessons' ) => void; // Function to handle tab changes
+    dynamicTabs: { label: string; value: 'students' | 'lessons'  }[]; // Dynamic tab options
 }
 
 const TabBar: React.FC<TabBarProps> = ({ activeTab, onTabChange, dynamicTabs }) => {
